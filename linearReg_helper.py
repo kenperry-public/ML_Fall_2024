be9ad2800b0bb992978cf2e3dcc6dd8da7e0b09d
Your Name <you@example.com>
2024-09-16 12:42:46 -0400

    Lessons 3 and 4

--- linearReg_helper.py
+++ linearReg_helper.py
@@ -67,7 +67,7 @@ class LinReg:
         
         return df
     
-    def plot(self, model, y, X, feature_names=[], showEquation=True, showTrue=True, title=None):
+    def plot(self, model, y, X, feature_names=[], showEquation=True, showTrue=True, title=None, visible=True):
         # Make predictions
         y_pred = model.predict(X)
         
@@ -81,32 +81,32 @@ class LinReg:
         fig, ax = plt.subplots(figsize=(10, 6))
         
         # Scatter plot of actual training data: x_1, y
-        ax.scatter(x_1, y, color='blue', alpha=0.5, label='Data points')
+        _= ax.scatter(x_1, y, color='blue', alpha=0.5, label='Data points')
         
         # Line plot fitted values (prediction)
         # Re-order the data in increasing value of x_1
         # - this is only so that the fitted values appear as a line
         sort_idx = x_1.argsort()
         x_1_sort, y_pred_sort = x_1[sort_idx], y_pred[sort_idx]
-        ax.plot(x_1_sort, y_pred_sort, color='red', label='Regression')
+        _= ax.plot(x_1_sort, y_pred_sort, color='red', label='Regression')
 
         # Enumerate features
         features_text = ", ".join(feature_names)
         
         # Set labels and title
-        ax.set_xlabel('$x_1$')
-        ax.set_ylabel('y')
+        _=  ax.set_xlabel('$x_1$')
+        _= ax.set_ylabel('y')
         
         title_string = title if title is not None else ""
         title_string += f'Features: {features_text}\nR-squared: {r_squared:.4f}'
         
-        ax.set_title(title_string)
-        ax.legend()
+        _= ax.set_title(title_string)
+        _= ax.legend()
 
         # Add text describing the true relationship
         if showTrue:
-            ax.text(0.05, 0.75, f'True relationship: {self.true_relationship()}', 
-                     transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+            _= ax.text(0.05, 0.75, f'True relationship: {self.true_relationship()}', 
+                       transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
 
         # Add text describing the model
         if showEquation:
@@ -115,15 +115,19 @@ class LinReg:
                 equation_terms.append(f"{model.coef_[i]:.2f}{feature_names[i]}")
 
             equation = f'Prediction: y = {" + ".join(equation_terms)} '
-            ax.text(0.05, 0.85, equation, transform=plt.gca().transAxes, 
-                    verticalalignment='top', fontsize=10)
+            _= ax.text(0.05, 0.85, equation, transform=plt.gca().transAxes, 
+                       verticalalignment='top', fontsize=10)
 
         _= plt.tight_layout()
-        _= plt.show()
+
+        if not visible:
+            _= plt.close(fig)
+        else:
+            _= plt.show()
 
         return fig, ax
 
-    def plot_pred_vs_true(self, model, y, X,  feature_names=[], showEquation=True,  showTrue=False, title=None):
+    def plot_pred_vs_true(self, model, y, X,  feature_names=[], showEquation=True,  showTrue=False, title=None, visible=True):
         # Make predictions
         y_pred = model.predict(X)
 
@@ -141,8 +145,8 @@ class LinReg:
 
         # Add text describing the true relationship
         if showTrue:
-            ax.text(0.05, 0.75, f'True relationship: {self.true_relationship()}', 
-                     transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+            _= ax.text(0.05, 0.75, f'True relationship: {self.true_relationship()}', 
+                       transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
 
         # Add text describing the model
         if showEquation:
@@ -151,8 +155,8 @@ class LinReg:
                 equation_terms.append(f"{model.coef_[i]:.2f}{feature_names[i]}")
 
             equation = f'Prediction: y = {" + ".join(equation_terms)} '
-            ax.text(0.05, 0.85, equation, transform=plt.gca().transAxes, 
-                     verticalalignment='top', fontsize=10)
+            _= ax.text(0.05, 0.85, equation, transform=plt.gca().transAxes, 
+                       verticalalignment='top', fontsize=10)
             
         # Create the title
         title_string = title if title is not None else ""
@@ -162,7 +166,88 @@ class LinReg:
         _= ax.legend()
 
         _= plt.tight_layout()
-        _= plt.show()
+
+        if not visible:
+            _= plt.close(fig)
+        else:
+            _= plt.show()
 
         return fig, ax
 
+    # Create figures for the lecture
+    def createFigs(self, visible=False):
+        # Some options to configure the data
+        rho, sigma_mult = 0, 20
+
+        # Generate the data
+        df = self.gen_data(samples=300, rho=rho, sigma_mult=sigma_mult)
+
+        # Separate the features from the target
+        X = df[ ["x_1", "x_2"]].to_numpy()
+        y = df[ "y"].to_numpy()
+
+        from sklearn.model_selection import train_test_split
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                           random_state=self.seed,  
+                                           test_size=0.2,  
+                                           shuffle=True) 
+
+        # Create the scatter plot of the examples
+        fig_data, ax_data = plt.subplots(figsize=(10, 6))
+
+        _= ax_data.scatter(X_train[:,0], y_train, color='blue', alpha=0.5, label='Data points')
+
+
+        # Set labels and title
+        _= ax_data.set_xlabel('$x_1$')
+        _= ax_data.set_ylabel('$y$')
+        _= ax_data.legend()
+
+        _= plt.tight_layout()
+
+        if not visible:
+            _= plt.close(fig_data)
+
+    
+        # Create plot of fitting to one variable
+        from sklearn.linear_model import LinearRegression
+
+        # Create and fit the linear regression model
+        model = LinearRegression()
+
+        # Note: we access the single feature as X_train[:, [9]] rather than X_train[:, 0] to ensure the result is 2D
+        # - with a singlteon final dimension, as required by sklearn
+        _= model.fit(X_train[:,[0] ], y_train)
+    
+        fig_x1, ax_x1 = self.plot(model, y_test, X_test[:,[0] ], feature_names=["$x_1$"],
+                                  title="Linear Regression\nOut of sample (test data)\n",
+                                  showTrue=False,
+                                  visible=visible
+                                  )
+
+    
+        if not visible:
+            _= plt.close(fig_x1)
+
+    
+        # Create plot of fit vs x_1, x_1^2
+        # Add an x_1^2 column to X_train AND X_test
+        X_train_with_sq = np.column_stack( (X_train[:, 0], X_train[:, 0]**2) )
+        X_test_with_sq = np.column_stack( (X_test[:, 0], X_test[:, 0]**2) )
+
+        # Fit the model
+        model = LinearRegression()
+        _= model.fit(X_train_with_sq, y_train)
+
+        fig_x1_x1sq, ax_x1_x1sq = self.plot(model, y_test, X_test_with_sq, feature_names=["$x_1$", "$x_1^2$"],
+                                            title="Linear Regression\nOut of sample (test data)\n",
+                                            showTrue=False,
+                                            visible=visible
+                                            )
+
+
+        if not visible:
+            _= plt.close(fig_x1_x1sq)
+        
+        return (fig_data, fig_x1, fig_x1_x1sq)
